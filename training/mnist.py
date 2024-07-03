99767441fc0b65030c39db4f38f1e8148498972a
sam <sam@thinky.ai>
2024-07-03 23:30:30 +0000

    Code cleanup for distillation work.

--- training/mnist.py
+++ training/mnist.py
@@ -426,7 +426,7 @@ def main(override=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_training_steps", type=int, default=10000)
     parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--config_path", type=str, default="configs/glide.yaml")
+    parser.add_argument("--config_path", type=str, required=True)
     parser.add_argument("--sample_with_guidance", action="store_true")
     parser.add_argument("--save_and_sample_every_n", type=int, default=100)
     parser.add_argument("--load_model_weights_from_checkpoint", type=str, default="")
